5b2b179cefc94f1211672c793e86d76e79935334
Craig Drummond <craig.p.drummond@gmail.com>
2020-07-20 06:13:32 +0100

    Move genre filtering into API server

--- MuslyMixer/Plugin.pm
+++ MuslyMixer/Plugin.pm
@@ -1,7 +1,7 @@
 package Plugins::MuslyMixer::Plugin;
 
 #
-# Musy 'Dont Stop The Music' mixer.
+# Musly 'Dont Stop The Music' mixer.
 #
 # (c) Craig Drummond, 2020
 #
@@ -35,7 +35,7 @@ use Plugins::MuslyMixer::Settings;
 my $initialized = 0;
 my $MuslyPort;
 my @genreSets = ();
-my $NUM_TRACKS = 20;
+my $NUM_TRACKS = 10;
 my $NUM_TRACKS_TO_USE = 5;
 my $NUM_SEED_TRACKS = 5;
 
@@ -57,15 +57,14 @@ sub initPlugin {
     return 1 if $initialized;
 
     $prefs->init({
-        filter_genres   => 1,
-        port            => 11000,
+        filter_genres => 1,
+        port          => 11000,
     });
 
     if ( main::WEBUI ) {
         Plugins::MuslyMixer::Settings->new;
     }
 
-    _initGenres();
     $initialized = 1;
     return $initialized;
 }
@@ -85,16 +84,11 @@ sub postinitPlugin {
             # don't seed from radio stations - only do if we're playing from some track based source
             # Get list of valid seeds...
             if ($seedTracks && ref $seedTracks && scalar @$seedTracks) {
-                my @seedGenres = ();
                 my @seedsToUse = ();
                 foreach my $seedTrack (@$seedTracks) {
                     my ($trackObj) = Slim::Schema->find('Track', $seedTrack->{id});
                     if ($trackObj) {
-                        my @genres = _getSeedGenres($trackObj->id);
-                        main::DEBUGLOG && $log->debug("Seed " . $trackObj->path . " id:" . $seedTrack->{id} . " genres:" . Data::Dump::dump(@genres));
-                        if (scalar @genres > 1) {
-                            push @seedGenres, @genres;
-                        }
+                        main::DEBUGLOG && $log->debug("Seed " . $trackObj->path . " id:" . $seedTrack->{id});
                         push @seedsToUse, $trackObj;
                     }
                 }
@@ -102,50 +96,20 @@ sub postinitPlugin {
                 if (scalar @seedsToUse > 0) {
                     my $mix = _getMix(\@seedsToUse);
                     main::idleStreams();
-
                     if ($mix && scalar @$mix) {
-                        if (scalar @seedGenres > 1) {
-                            # One or more seed tracks genres are listed in genres.json, so only allow similar genres
-                            my %genrehash = map { $_ => 1 } @seedGenres;
-                            foreach my $candidate (@$mix) {
-                                my @cgenres = _getCandidateGenres($candidate);
-                                my $count = scalar @cgenres;
-                                my $found = 0;
-
-                                for (my $i = 0; $i < $count && $found==0; $i++) {
-                                    if (exists($genrehash{$cgenres[$i]})) {
-                                        main::DEBUGLOG && $log->debug($candidate . " matched on genre " . $cgenres[$i]);
-                                        push @$tracks, $candidate;
-                                        $found=1;
-                                    }
-                                }
-
-                                $found==0 && main::DEBUGLOG && $log->debug($candidate . " FAILED to match genre");
-
-                                # Stop processing if we have enough acceptable tracks...
-                                if ($found==1 && scalar(@$tracks)>=($NUM_TRACKS_TO_USE*2)) {
-                                    main::DEBUGLOG && $log->debug("Have sufficient tracks");
-                                    last;
-                                }
-                            }
-                        } else {
-                            # Seed track genres are not in genres.json - so can't filter!
-                            push @$tracks, @$mix;
-                        }
+                        push @$tracks, @$mix;
                     }
                 }
             }
 
+            # Remove duplicates...
             $tracks = Slim::Plugin::DontStopTheMusic::Plugin->deDupe($tracks);
-            # If we have more than 2*num tracks, then use 1st 2*num - and then shuffle those...
-            if ( scalar @$tracks > ($NUM_TRACKS_TO_USE*2) ) {
-                $tracks = [ splice(@$tracks, 0, $NUM_TRACKS_TO_USE*2) ];
-            }
+            # If we have more than num tracks, then use 1st num...
             if ( scalar @$tracks > $NUM_TRACKS_TO_USE ) {
-                Slim::Player::Playlist::fischer_yates_shuffle($tracks);
                 $tracks = [ splice(@$tracks, 0, $NUM_TRACKS_TO_USE) ];
             }
-
+            # Shuffle tracks...
+            Slim::Player::Playlist::fischer_yates_shuffle($tracks);
             $cb->($client, $tracks);
         });
     }
@@ -169,9 +133,9 @@ sub _getMix {
     my $res;
 
     my %args = (
-            # Set the size of the list
-            'count'  => $NUM_TRACKS,
-            'format' => 'text'
+            'count'       => $NUM_TRACKS,
+            'format'      => 'text',
+            'filtergenre' => $prefs->get('filter_genres')
         );
 
     my $argString = join( '&', map { "$_=$args{$_}" } keys %args );
@@ -218,92 +182,6 @@ sub _syncHTTPRequest {
     my $http = LWP::UserAgent->new;
     $http->timeout($prefs->get('timeout') || 5);
     return $http->get("http://localhost:$MuslyPort$url");
-}
-
-sub _initGenres {
-    my $filePath = Slim::Utils::Prefs::dir() . "/genres.json";
-    if (! -e $filePath) {
-        $filePath = dirname(__FILE__) . "/genres.json";
-    }
-    
-    my $json = read_file($filePath);
-    my $data = decode_json($json);
-    @genreSets = ();
-    if ($data) {
-        my $dbh = Slim::Schema->dbh;
-        my $sql = $dbh->prepare_cached( qq{SELECT genres.id FROM genres WHERE name = ? LIMIT 1} );
-        foreach my $s (@$data) {
-            my $set={};
-            my $count = 0;
-            foreach my $genre (@$s) {
-                $sql->execute($genre);
-                if ( my $result = $sql->fetchall_arrayref({}) ) {
-                    my $val = $result->[0]->{'id'} if ref $result && scalar @$result;
-                    if ($val) {
-                        $set->{$val}=1;
-                        $count++;
-                    }
-                }
-            }
-
-            if ($count>1) {
-                push(@genreSets, $set);
-            }
-        }
-    }
-}
-
-sub _getSeedGenres {
-    my $track = shift;
-    my @genres = ();
-    if ($prefs->get('filter_genres')) {
-        my @lmsgenres = ();
-        # Get genres stored in LMS for this track
-        my $dbh = Slim::Schema->dbh;
-        my $sql = $dbh->prepare_cached( qq{SELECT genre FROM genre_track WHERE track = ?} );
-        $sql->execute($track);
-        if ( my $result = $sql->fetchall_arrayref({}) ) {
-            if (ref $result && scalar @$result) {
-                foreach my $r (@$result) {
-                    push (@lmsgenres, $r->{'genre'})
-                }
-            }
-        }
-
-        foreach my $lmsgenre (@lmsgenres) {
-            for my $href (@genreSets) {
-                my %hash = %$href;
-                if (exists($hash{$lmsgenre})) {
-                    push (@genres, keys %hash); 
-                }
-            }
-        }
-    }
-
-    return @genres;
-}
-
-sub _getCandidateGenres {
-    my $url = shift;
-    my @genres = ();
-    my $dbh = Slim::Schema->dbh;
-    my $sql = $dbh->prepare_cached( qq{SELECT id FROM tracks WHERE url = ? LIMIT 1} );
-    $sql->execute($url);
-    if ( my $result = $sql->fetchall_arrayref({}) ) {
-        if (ref $result && scalar @$result) {
-            my $sql = $dbh->prepare_cached( qq{SELECT genre FROM genre_track WHERE track = ?} );
-            $sql->execute($result->[0]->{'id'});
-            if ( my $result = $sql->fetchall_arrayref({}) ) {
-                if (ref $result && scalar @$result) {
-                    foreach my $r (@$result) {
-                        push (@genres, $r->{'genre'})
-                    }
-                }
-            }
-        }
-    }
-    main::DEBUGLOG && $log->debug("Candidate " . $url . " genres: " . Data::Dump::dump(@genres));
-    return @genres;
 }
 
 1;
