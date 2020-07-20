d5aca4c5d64fc77aee688828f58c5af05976da2a
Craig Drummond <craig.p.drummond@gmail.com>
2020-07-20 16:09:09 +0100

    Only use 5 out of (max) 15 returned tracks. Shuffle and take top 5 - will introduce more variety.

--- MuslyMixer/Plugin.pm
+++ MuslyMixer/Plugin.pm
@@ -36,7 +36,7 @@ my $initialized = 0;
 my $MuslyPort;
 my @genreSets = ();
 my $NUM_TRACKS = 15;
-my $NUM_TRACKS_TO_USE = 10;
+my $NUM_TRACKS_TO_USE = 5;
 my $NUM_SEED_TRACKS = 5;
 my $IGNORE_LAST_TRACKS = 50;
 
